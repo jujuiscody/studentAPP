0828d545b9d1ebdbc06b02d90d252e073d485b86
Raju <raju.raj408@gmail.com>
2018-04-07 23:48:05 -0500

    crud operations done

--- src/main/java/com/app/demo/Controller/CustomerController.java
+++ src/main/java/com/app/demo/Controller/CustomerController.java
@@ -1,8 +1,14 @@
 package com.app.demo.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.demo.model.Customer;
@@ -13,6 +19,39 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
+	
+	// get all Customers 
+	@GetMapping("/customers")
+	//@RequestMapping(method=RequestMethod.GET)
+	public List<Customer> getAllCustomer(){
+		return customerService.getAllCustomers();
+	}
+	
+	//  get Customer referring through ID
+	@GetMapping("/customers/{id}")
+	//@RequestMapping(method=RequestMethod.GET, value="/{id}")
+	public Customer getByID(@PathVariable Long id) {
+		return customerService.getByID(id);
+	}
+	
+	// creating customer by request
+	@PostMapping("/customers")
+	//@RequestMapping(method=RequestMethod.POST)
+	public Customer create(@RequestBody Customer customer) {
+		return customerService.addCustomer(customer);
+	}
+	
+	// updating existing customer
+	@PutMapping("/customers/{id}")
+	public Customer updateCustomer(@PathVariable long id, @RequestBody Customer customer) throws Exception{
+		customer.setId(id);
+		return customerService.modifyCustomer(id, customer);
+	}
+	
+	@DeleteMapping("/customers/{id}")
+	public void deleteCustomer(@PathVariable long id) throws Exception {
+		this.customerService.deleteCustomer(id);
+	}
 	
 	
 }
--- src/main/java/com/app/demo/service/CustomerService.java
+++ src/main/java/com/app/demo/service/CustomerService.java
@@ -1,8 +1,11 @@
 package com.app.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.demo.model.Customer;
 import com.app.demo.repositry.CustomerRepositry;
 
 @Service
@@ -11,4 +14,37 @@ public class CustomerService {
 	@Autowired
 	private CustomerRepositry customerRepositry;
 
+	// getting all customer details from database
+	public List<Customer> getAllCustomers(){
+		return customerRepositry.findAll();
+	}
+	
+	// getting single customer from database
+	public Customer getByID(Long id) {
+		return customerRepositry.findOne(id);
+	}
+	
+	// adds customer to database 
+	public Customer addCustomer(Customer customer) {
+		return customerRepositry.save(customer);
+	}
+	
+	public Customer modifyCustomer(Long id, Customer customer) throws Exception {
+		
+		Customer dbCustomer = this.getByID(id);
+		
+		if(dbCustomer == null) {
+			throw new Exception("customer id: "+id+" is not found");
+		}
+		return customerRepositry.save(customer);
+	}
+	
+	public void deleteCustomer(long id) throws Exception{
+		Customer dbCustomer = this.getByID(id);
+		if(dbCustomer == null) {
+			throw new Exception("customer id: "+id +" is not found");
+		}
+		this.customerRepositry.delete(id);
+		
+	}
 }
